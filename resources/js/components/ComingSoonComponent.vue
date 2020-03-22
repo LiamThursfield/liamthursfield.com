691c4f0c2f7e6fbe2843a3ba207b374f4e3fd565
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-03-22 13:25:02 +0000

    Add d-script

--- resources/js/components/ComingSoonComponent.vue
+++ resources/js/components/ComingSoonComponent.vue
@@ -1,18 +1,30 @@
 <template>
     <div class="bg-gray-900 flex h-screen items-center justify-center text-mono-100 w-screen">
-        <div class="p-3 text-center">
+        <div class="p-3 text-center md:max-w-3xl md:w-full">
             <h1 class="text-4xl text-blue-700 md:text-6xl">Liam Thursfield</h1>
-            <h2 class="text-xl text-gray-600 md:text-3xl">Coming soon...</h2>
+            <h2 class="text-xl text-gray-600 md:text-3xl">Website coming soon...</h2>
 
             <div class="
-                    flex flex-col -mx-1 mt-6 w-full
+                    border-gray-800 border-t flex flex-col -mx-1 mt-6 pt-6  w-full
                     md:flex-row
                 "
             >
                 <a
                     class="
-                        block border border-gray-600 rounded m-1 px-3 py-2 text-gray-600 w-full
-                        md:w-1/2
+                        block border border-gray-600 flex items-center justify-center rounded m-1 px-3 py-2 text-gray-600 w-full
+                        md:w-1/3
+                        hover:bg-blue-700 hover:border-blue-700 hover:text-gray-900
+                    "
+                    href="https://d-script.liamthursfield.com"
+                    target="_blank"
+                >
+                    d-script
+                </a>
+
+                <a
+                    class="
+                        block border border-gray-600 flex items-center justify-center rounded m-1 px-3 py-2 text-gray-600 w-full
+                        md:w-1/3
                         hover:bg-blue-700 hover:border-blue-700 hover:text-gray-900
                     "
                     href="https://fortnite-tracker.liamthursfield.com"
@@ -23,11 +35,11 @@
 
                 <a
                     class="
-                        block border border-gray-600 rounded m-1 px-3 py-2 text-gray-600 w-full
-                        md:w-1/2
+                        block border border-gray-600 flex items-center justify-center rounded m-1 px-3 py-2 text-gray-600 w-full
+                        md:w-1/3
                         hover:bg-blue-700 hover:border-blue-700 hover:text-gray-900
                     "
-                    href="https://fortnite-tracker.liamthursfield.com"
+                    href="https://youtube-playlist-details.liamthursfield.com"
                     target="_blank"
                 >
                     Youtube Playlist Details
